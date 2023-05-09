dd59bf8cbb3e2f14861314ce51e3a6e6441e54f3
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2023-05-09 16:37:51 -0300

    add mock file

--- src/components/scrollProfiles/index.tsx
+++ src/components/scrollProfiles/index.tsx
@@ -1,31 +1,7 @@
 import { FlatList, StyleSheet } from "react-native"
 import { width } from "../../util/dimensions"
 import { ProfileButton } from "../profileButton"
-
-// type runnerType = {id:string, name: string, checked:boolean}
-const dataMock = [
-  { id: "00", name: "Relâmpago", checked: true },
-  { id: "01", name: "Agente Tom", checked: false },
-  { id: "02", name: "Doc Hudson", checked: false },
-  { id: "03", name: "Cruz Ramirez", checked: false },
-  { id: "04", name: "Cruz Ramirez", checked: false },
-  { id: "05", name: "Cruz Ramirez", checked: false },
-  { id: "06", name: "Cruz Ramirez", checked: true },
-  { id: "07", name: "Cruz Ramirez", checked: false },
-  { id: "08", name: "Cruz Ramirez", checked: false },
-  { id: "09", name: "Cruz Ramirez", checked: false },
-  { id: "10", name: "Cruz Ramirez", checked: false },
-  { id: "11", name: "Cruz Ramirez", checked: true },
-  { id: "12", name: "Cruz Ramirez", checked: false },
-  { id: "13", name: "Cruz Ramirez", checked: false },
-  { id: "14", name: "Cruz Ramirez", checked: false },
-  { id: "15", name: "Cruz Ramirez", checked: false },
-  { id: "16", name: "Cruz Ramirez", checked: false },
-  { id: "17", name: "Cruz Ramirez", checked: false },
-  { id: "18", name: "Cruz Ramirez", checked: false },
-  { id: "19", name: "Cruz Ramirez", checked: false },
-  { id: "20", name: "Cruz Ramirez", checked: true },
-]
+import { dataMock } from "../../mock/runners"
 
 interface ScrollProfileProps {
   runnerNameInput: string
